a85caeadd479fb91ae000d14239bb671d851d5f0
gsdkira <larreakelly@gmail.com>
2022-05-14 22:09:58 -0400

    added a list item

---
+++ src/components/Feedbackitem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Feedbackitem() {
+  return (
+    <div className='card'>
+        <div className="num-display"></div>
+        <div className="text-display">
+            This is an example of a feedback item
+        </div>
+        </div>
+  )
+}
+
+export default Feedbackitem
